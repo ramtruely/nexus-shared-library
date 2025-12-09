eaafa304427fd36044c1d562db490e5bef30410b
ramtruely <ramtruely6@gmail.com>
2025-12-09 08:36:26 +0530

    Update examplePipeline.groovy

--- vars/examplePipeline.groovy
+++ vars/examplePipeline.groovy
@@ -1,18 +1,6 @@
+def deployApp() {
+    echo 'deplying the application...'
+    echo "deploying with nexus retrieval"
+} 
 
-
-def call() {
-  node() {
-
-    stage('Simple test') {
-      sh "echo 'Hello World'"
-    }
-
-    stage('Maven version') {
-      MVN_HOME = tool name: 'Maven 3.9.0', type: 'maven'
-      withEnv(["PATH+MVN=${MVN_HOME}/bin"]) {
-        sh 'mvn --version'
-      }
-    }
-
-  }
-}
+return this
