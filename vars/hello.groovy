de4feabfdc1c85a064176baea6d4ddf0e2ddea3f
ramtruely <ramtruely6@gmail.com>
2025-12-09 10:01:51 +0530

    Create hello.groovy

---
+++ vars/hello.groovy
@@ -0,0 +1,6 @@
+def call(String name = "World") {
+    echo "👋 Hello ${name} from Nexus Shared Library!"
+    echo "Build: ${env.BUILD_NUMBER} | Node: ${env.NODE_NAME}"
+    return "SUCCESS-${env.BUILD_NUMBER}"
+}
+return this
