1e06edbe66364b85c23b15ac8b580b684171220f
ramtruely <ramtruely6@gmail.com>
2025-12-14 18:32:31 +0530

    Delete src directory

--- src/com/example/DeploymentUtils.groovy
+++
@@ -1,8 +0,0 @@
-package com.example
-
-class DeploymentUtils {
-    static void deployToEnv(String app, String version, String env = 'prod') {
-        println "🌐 Deploying ${app}-${version} to ${env}.example.com"
-        println "📦 Target: ${env}.example.com:8080/${app}"
-    }
-}
