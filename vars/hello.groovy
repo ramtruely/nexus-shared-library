88568e26daa2cd8d7367622a0b614e207170acd9
ramtruely <ramtruely6@gmail.com>
2025-12-23 20:47:08 +0530

    Delete vars directory

--- vars/hello.groovy
+++
@@ -1,8 +0,0 @@
-def call(String name = "World") {
-    def buildInfo = com.example.BuildInfo.buildDetails
-    def nodeInfo = com.example.BuildInfo.nodeInfo
-    
-    echo "👋 Hello ${name} from Nexus Shared Library!"
-    echo "${buildInfo} | Node: ${nodeInfo}"
-    return "SUCCESS-${env.BUILD_NUMBER}"
-}
